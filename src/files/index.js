194be3ddd994fc14550e6b1e3ad6cddd384a2b7e
Anh Nguyen <anh.nguyen11@hitachivantara.com>
2023-04-14 16:19:04 +0700

    Fix

--- src/files/index.js
+++ src/files/index.js
@@ -1,7 +1,3 @@
-import txt1 from "./D1.txt";
-import txt2 from "./D2.txt";
-import txt3 from "./D3.txt";
-import txt4 from "./D4.txt";
 import az from "./az.txt";
 
-export { txt1, txt2, txt3, txt4, az };
+export {az };
